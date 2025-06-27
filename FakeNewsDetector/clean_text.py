96fe611835cc0a2dbf6a577442c433e074a4ad94
dhruvin6122 <pateldhruvin0612@gmail.com>
2025-06-27 13:03:52 +0530

    Add files via upload

---
+++ FakeNewsDetector/clean_text.py
@@ -0,0 +1,20 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text)
+    text = re.sub(r"[^a-z\s]", '', text)
+    words = nltk.word_tokenize(text)
+    cleaned = [lemmatizer.lemmatize(w) for w in words if w not in stop_words]
+    return ' '.join(cleaned)
